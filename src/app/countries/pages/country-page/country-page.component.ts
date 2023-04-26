c7bb059a948e80bb78857b990b8aed8dbb2325eb
Gianfranco_Vives <gianfranco.vives.03@gmail.com>
2023-04-26 11:44:36 -0500

    Cambios mostrar la información del país seleccionado

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Country, Translation } from '../../interfaces/country';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -7,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class CountryPageComponent implements OnInit {
-
-  constructor() { }
+  country!: Country |null;
+  translations!: Translation[];
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private service: CountriesService
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id})=>this.service.searchCountryByAlphaCode(id))
+    )
+    .subscribe(country=>{
+      if(!country){
+        return this.router.navigateByUrl('');
+      }
+      this.country = country;
+      this.translations = Object.values(country!.translations);
+      return;
+        }
+      )
+    }
   }
 
-}
+
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Country } from '../interfaces/country';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
@@ -10,6 +10,16 @@ export class CountriesService {
     constructor(
         private httpClient: HttpClient
     ) { }
+
+    searchCountryByAlphaCode(code: string): Observable<Country | null>{
+
+        return this.httpClient.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
+               .pipe(
+                delay(900),
+                map(countries=> countries.length>0? countries[0]: null),
+                catchError(()=>of(null))
+               );
+    }
     
     searchCapital(query:string): Observable<Country[]>{
         return this.httpClient.get<Country[]>(`${this.apiUrl}/capital/${query}`)
