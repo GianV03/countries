78fb5adde05d39eb6b3937260235419abba4eb48
Giangranco Vives <gianfranco.vives.03@gmail.com>
2023-07-28 16:00:25 -0500

    Cambios en estilos y eliminación de componentes no usados

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
-import { ContactComponent } from './pages/contact/contact.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent,
-    AboutPageComponent,
     SidebarComponent,
-    ContactComponent,
     SearchBoxComponent
   ],
   imports: [
@@ -22,10 +16,7 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     RouterModule
   ],
   exports:[
-    HomePageComponent,
-    AboutPageComponent,
     SidebarComponent,
-    ContactComponent,
     SearchBoxComponent
   ]
 })
