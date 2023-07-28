b6e0c1bf245a6ab26705e35a571f36e2235a2a13
gianfranco vives <gianfranco.vives.03@gmail.com>
2023-07-28 13:17:00 -0500

    Cambio en el color de fondo del sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -5,9 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styles: [
     `
-    .sidebar-container{
-      background-color:black;
-    }
     li:hover{
       cursor:pointer
     }
